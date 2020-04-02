bc0a1e35ad4ee66ae123bdd9d366a678dd39eebb
alpha-acrobat-9971 <qsmity@gmail.com>
2020-04-02 07:00:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+var getFirstSelector = function(selector){
+ return  document.querySelector(selector)
+}
 
+
+var nestedTarget = function(){
+ return document.getElementById("nested").querySelector(".target")
+}
+
+
+var increaseRankBy = function(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < lis.length; i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
+
+var deepestChild = function(){
+ var childList = document.querySelector('div#grand-node')
+ var current = []
+ 
+ while(childList.querySelector('div')){
+   childList = childList.querySelector('div')
+   current.push(childList)
+ }
+ 
+ return current.pop()
+}
