c5540aaba3c8f8c457ef991b718c3904bcc71578
alpha-acrobat-9971 <qsmity@gmail.com>
2020-04-02 03:23:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+var getFirstSelector = function(selector){
+  document.querySelector('h1')
+}
 
+
+var nestedTarget = function(){
+ document.querySelector("div #nested .target ")
+}
+
+
+var increaseRankBy = function(n){
+  document.querySelectorAll("")
+}
